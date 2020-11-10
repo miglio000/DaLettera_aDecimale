c8b8553127a3b7a448f6926c1f69a54432fb23b5
miglio000 <gabrielemiglio23@gmail.com>
2020-11-10 12:25:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DaLettera_aDecimale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char lettera;
+            ushort decimale = 0;
+            Console.WriteLine("Inserire la lettera da convertire in ASCII");
+            lettera = Convert.ToChar(Console.ReadLine());
+            decimale = lettera;
+            Console.WriteLine($"La lettera: {lettera} in conversione ASCII è {decimale}");
+
+        }
+    }
+}
