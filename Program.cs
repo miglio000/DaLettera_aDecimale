fb9b1494bd9494355e7139844faad08d13366ef4
miglio000 <gabrielemiglio23@gmail.com>
2020-11-17 12:09:50 +0100

    decimale a lettera con riconversione

--- Program.cs
+++ Program.cs
@@ -14,7 +14,11 @@ namespace DaLettera_aDecimale
             Console.WriteLine($"The letter: {letter} converted to ASCII is {decimale}");
             string hexadecimal = decimale.ToString("X");
             Console.WriteLine($"The decimal number converted to hexadecimal is:{hexadecimal}");
-
+            Console.WriteLine("Now try the reconversion.");
+            Console.WriteLine("Insert a Decimal Number:");
+            ushort decimale2 = Convert.ToUInt16(Console.ReadLine());
+            char character = Convert.ToChar(decimale2);
+            Console.WriteLine($"The character is {character}");
         }
     }
 }
